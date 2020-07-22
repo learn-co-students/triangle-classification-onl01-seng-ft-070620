350a2145b221c46aa52ba17b102c847b6162f037
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-21 21:47:18 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,19 +12,19 @@ class Triangle
   end 
 
   def kind
-    if self.l1 == self.l2 && self.l1 == self.l3
+    if self.l1 < 0 || self.l2 < 0 || self.l3 < 0
+      raise TriangleError 
+    elsif self.l1 == 0 && self.l2 = 0 && self.l3 = 0
+      raise TriangleError
+    elsif self.l1 + self.l2 <= self.l3 || self.l2 + self.l3 <= self.l1 || self.l1 + self.l3 <= self.l2 
+      raise TriangleError
+    elsif self.l1 == self.l2 && self.l1 == self.l3
       "equilateral".to_sym
     elsif self.l1 == self.l2 || self.l1 == self.l3 || self.l2 == self.l3 
       "isosceles".to_sym 
     elsif self.l1 != self.l2 && self.l2 != self.l3 
       "scalene".to_sym 
-    elsif self.l1 < 0 || self.l2 < 0 || self.l3 < 0
-      raise TriangleError 
-    elsif self.l1 == 0 && self.l2 = 0 && self.l3 = 0
-      raise TriangleError
-    elsif self.l1 + self.l2 < self.l3 || self.l2 + self.l3 < self.l3 || self.l1 + self.l3 < self.l2 
-      raise TriangleError
-    end 
+  end 
   end 
 
   class TriangleError < StandardError
